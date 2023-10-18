70db88dd80a3f83833235cb97dd077e9f95123bb
joseph lyons <jlyons3@ramapo.edu>
2023-10-17 21:11:32 -0400

    Updated about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -27,16 +27,14 @@ export default function About() {
           <div className={styles.topContainer}>
             <h1 className={styles.header}>hey, this is Leah <span></span></h1>
             <h1 className={styles.subHeader}>thanks for lookin'</h1>
-            <p className={styles.content}> I’m a Product Designer solving business problems, and I tend to believe that there are not any problems that cannot be solved. 
-              My background as a Special Education Teacher gives me tremendous patience and a willingness to persevere and find a way. For me, 
-              design is no different.
+            <p className={styles.content}> I’m a Product Designer solving business problems. I tend to believe that there are not any problems that cannot be solved. My background as a Special Education Teacher gives me tremendous patience and a willingness to persevere and find a way. 
             </p>
-            <p className={styles.content}> My fatal flaw is asking a lot of questions. I’ve spent my time as a designer listening to clients and users,
-             learning from their feedback, and asking questions for clarification and verification, all in service of creating the best possible 
-             digital interface that they love and users want to explore. 
+            <p className={styles.content}>For me, design is no different.</p>
+            <p className={styles.content}> 
+              My super power is asking a lot of questions. I’ve spent my time as a designer listening to clients and users, learning from their feedback, and asking questions for clarification and verification, all in service of creating the best possible digital interface that they love and users want to explore.
             </p>
-            <p className={styles.content}> When I’m not pushing pixels on a screen, you can find me riding my bike 
-             across the five boroughs, reading, or in some sort of body of water, playing and living my best mermaid life.
+            <p className={styles.content}> 
+              When I’m not pushing pixels on a screen, you can find me riding my bike across the five boroughs, reading, or in some sort of body of water, playing and living my best mermaid life.
             </p>
             <Link className={styles.resumeLink} target="_blank" href="/Resume-Leah B Annitto.pdf">Take a look at my resume.</Link>
           </div>
