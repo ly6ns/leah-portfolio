76c011db115b0560e3764bf43174f588066935ff
joseph lyons <jlyons3@ramapo.edu>
2023-10-23 20:59:29 -0400

    Updated font on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,7 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
         <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500&display=swap" rel="stylesheet"></link>
       </Head>
       <main>
         <div>
